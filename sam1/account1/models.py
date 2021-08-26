6617163eacc6e2a2aebd7ce52099facdbd098c13
anjitha <anjitha1998pvr@gmail.com>
2021-08-26 21:55:03 +0530

    sample1file added

---
+++ sam1/account1/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Contact1(models.Model):
+    name=models.CharField(max_length=100,null=True,blank=True)
+    email=models.EmailField(max_length=200,null=True,blank=True)
+    number=models.CharField(max_length=10,null=True,blank=True)
+    def __str__(self):
+        return self.name
